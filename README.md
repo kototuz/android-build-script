001f3eb210d61567e03f563f091bcec67d8ab87c
kototuz <ivann1812@gmail.com>
2025-02-18 14:57:37 +0300

    Make project structure more easier

--- README.md
+++ README.md
@@ -21,26 +21,22 @@ but the project doesn't use NDK. The project is a template for creating java and
 ### Step 1
 
 Install [command line tools](https://developer.android.com/studio/#command-tools) (At the page bottom).
-After installing decompress the file into `android/sdk`. Make sure there is a `cmdline-tools` folder. 
+After installing decompress the file into `sdk` folder. Make sure there is a `cmdline-tools` folder in the `sdk` folder. 
 
 ### Step 2
 
 Just run this script it will install SDK and generate **keystore**:
 
 ``` shell
-cd android/sdk/cmdline-tools/bin
+cd sdk/cmdline-tools/bin
 ./sdkmanager --update --sdk_root=../..
 ./sdkmanager --install "build-tools;29.0.3" --sdk_root=../..
 ./sdkmanager --install "platform-tools" --sdk_root=../..
 ./sdkmanager --install "platforms;android-29" --sdk_root=../..
-cd ../../../..
+cd ../../..
 
-cd android
-keytool -genkeypair -validity 1000 -dname "CN=project,O=Android,C=ES" -keystore project.keystore -storepass 'project' -keypass 'project' -alias projectKey -keyalg RSA
-cd ..
-
-mkdir -p android/build/assets
-mkdir -p android/build/dex
+mkdir -p build
+keytool -genkeypair -validity 1000 -dname "CN=project,O=Android,C=ES" -keystore ./build/project.keystore -storepass 'project' -keypass 'project' -alias projectKey -keyalg RSA
 ```
 
 ## Usage
@@ -54,5 +50,5 @@ To build the app (it means generate an apk file) just run this:
 ./build.sh
 ```
 
-The main file is `android/build/src/com/template/project/MainActivity.java` (why is it so long?).
+The main file is `src/com/template/project/MainActivity.java` (why is it so long?).
 You can start write your app there.
--- build.sh
+++ build.sh
@@ -3,34 +3,36 @@
 # stop on error and display each command as it gets executed. Optional step but helpful in catching where errors happen if they do.
 set -xe
 
-BUILD_TOOLS=android/sdk/build-tools/29.0.3
+BUILD_TOOLS=sdk/build-tools/29.0.3
+
+mkdir -p build build/dex
 
 $BUILD_TOOLS/aapt package -f -m \
-	-S android/build/res -J android/build/src -M android/build/AndroidManifest.xml \
-	-I android/sdk/platforms/android-29/android.jar
+	-S ./res -J ./src -M ./AndroidManifest.xml \
+	-I sdk/platforms/android-29/android.jar
 
 # Compile MainActivity.java
-javac -verbose -source 1.8 -target 1.8 -d android/build/obj \
+javac -verbose -source 1.8 -target 1.8 -d ./build/obj \
 	-bootclasspath jre/lib/rt.jar \
-	-classpath android/sdk/platforms/android-29/android.jar:android/build/obj \
-	-sourcepath src android/build/src/com/template/project/R.java \
-	android/build/src/com/template/project/MainActivity.java
+	-classpath sdk/platforms/android-29/android.jar:./build/obj \
+	-sourcepath src ./src/com/template/project/R.java \
+	./src/com/template/project/MainActivity.java
 
-$BUILD_TOOLS/dx --verbose --dex --output=android/build/dex/classes.dex android/build/obj
+$BUILD_TOOLS/dx --verbose --dex --output=./build/dex/classes.dex ./build/obj
 
 # Add resources and assets to APK
 $BUILD_TOOLS/aapt package -f \
-	-M android/build/AndroidManifest.xml -S android/build/res -A android/build/assets \
-	-I android/sdk/platforms/android-29/android.jar -F app.apk android/build/dex
+	-M ./AndroidManifest.xml -S ./res -A ./assets \
+	-I sdk/platforms/android-29/android.jar -F ./build/app.apk ./build/dex
 
 # Zipalign APK and sign
 # NOTE: If you changed the storepass and keypass in the setup process, change them here too
-$BUILD_TOOLS/zipalign -f 4 app.apk game.final.apk
-mv -f game.final.apk app.apk
+$BUILD_TOOLS/zipalign -f 4 ./build/app.apk game.final.apk
+mv -f game.final.apk ./build/app.apk
 
 # Install apksigner with `sudo apt install apksigner`
-apksigner sign  --ks android/project.keystore --out my-app-release.apk --ks-pass pass:project app.apk
-mv my-app-release.apk app.apk
+apksigner sign  --ks ./build/project.keystore --out ./build/my-app-release.apk --ks-pass pass:project ./build/app.apk
+mv ./build/my-app-release.apk ./build/app.apk
 
 # Install to device or emulator
-android/sdk/platform-tools/adb install -r app.apk
+sdk/platform-tools/adb install -r ./build/app.apk
